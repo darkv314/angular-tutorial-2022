d7f8b8dc5b9ea5fa8af027fdc25a8faee6ad87cf
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-05 18:18:16 -0400

    05-08-2022 Task 2 Examples of functions find, map and forEach Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,15 @@ export class AppComponent {
 
   constructor() {
     console.log('Hi');
+
+    const auxMap = [1, 2, 3, 4, 5].map((num) => num + 2);
+    console.log('auxMap: ', auxMap);
+
+    const auxForEach = [1, 2, 3, 4, 5].forEach((num) => num + 2);
+    console.log('auxForEach: ', auxForEach);
+
+    const auxFind = [1, 2, 3, 4, 5].find((num) => num === 6);
+    console.log('autoFind: ', auxFind);
   }
 
   // Arrow function examples
