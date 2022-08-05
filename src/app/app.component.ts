c95fd2ce073adcd4326a49e1a7ff33fe318d9989
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-05 18:35:52 -0400

    05-08-2022 Task3 Examples of join, split, filter, indexOf and findIndex functions Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,24 @@ export class AppComponent {
 
     const auxFind = [1, 2, 3, 4, 5].find((num) => num === 6);
     console.log('autoFind: ', auxFind);
+
+    const auxFindIndex = [1, 2, 3, 4, 5].findIndex((num) => num === 5);
+    console.log('auxFindIndes: ', auxFindIndex);
+
+    const auxIndexOf = [1, 2, 3, 4, 5].indexOf(5);
+    console.log('auxIndexOf: ', auxIndexOf);
+
+    const auxIndexOfString = 'Hola mundo Test'.indexOf('Test');
+    console.log('auxIndexOfString: ', auxIndexOfString);
+
+    const auxFilter = [1, 2, 3, 4, 5].filter((num) => num % 2 === 0);
+    console.log('auxFilter: ', auxFilter);
+
+    const auxJoin = [1, 2, 3, 4, 5].join('*****');
+    console.log('auxJoin: ', auxJoin);
+
+    const auxSplit = 'Hola, mundo, test'.split(',');
+    console.log('auxSplit: ', auxSplit);
   }
 
   // Arrow function examples
