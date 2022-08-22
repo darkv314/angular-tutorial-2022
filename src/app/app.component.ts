286c6ca8bfd175bba34d54d1219974c7454d3cc2
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-21 21:46:57 -0400

    Task 2 Behaviour Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs';
+import { BehaviorSubject, of, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -10,11 +10,50 @@ import { map } from 'rxjs/operators';
 export class AppComponent {
   title = 'test';
 
-  public data$ = of('hola');
+  public data$ = of('Hola');
+
+  public youtube$ = new BehaviorSubject<number>(0);
+  public tiktok$ = new BehaviorSubject<number>(0);
+  public twitter$ = new BehaviorSubject<number>(0);
+
+  public person1!: Subscription;
+  public person2!: Subscription;
 
   constructor() {
     this.data$.pipe(map((d) => d + '-map')).subscribe((res) => {
       console.log('Soy suscriptor', res);
     });
+
+    this.youtube$.subscribe(res => {
+      console.log('youtube subscription: ', res);
+    })
+
+    this.person1 = this.tiktok$.subscribe(res => {
+      console.log('Person1 tiktok subscription: ', res);
+    });
+
+    this.person2 = this.tiktok$.subscribe(res => {
+      console.log('Person2 tiktok subscription: ', res);
+    });
+
+    this.twitter$.subscribe(res => {
+      console.log('Twitter subscription: ', res);
+    });
+  }
+
+  addVideo() {
+    this.youtube$.next(1);
+  }
+
+  addTikTok() {
+    this.tiktok$.next(2);
+  }
+
+  addTweet() {
+    this.twitter$.next(3);
+  }
+
+  deleteTikTok() {
+    this.person1.unsubscribe();
   }
 }
