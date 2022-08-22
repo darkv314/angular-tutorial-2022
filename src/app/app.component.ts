ce0404932556ea1ed5b45ab82c5a7f944dfee099
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-21 21:22:05 -0400

    Task 1 Observable OF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
+
+  public data$ = of('hola');
+
+  constructor() {
+    this.data$.pipe(map((d) => d + '-map')).subscribe((res) => {
+      console.log('Soy suscriptor', res);
+    });
+  }
 }
