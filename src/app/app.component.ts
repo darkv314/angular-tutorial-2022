2b30bb7f9101145171cb229a918844a5e0913345
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-22 03:09:13 -0400

    Task 1 Use viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
+
+  @ViewChild('myText') myTest!: ElementRef;
+
+  send(data: any) {
+    console.log('data: ', data.value);
+    console.log('My test in ts: ', this.myTest);
+    this.myTest.nativeElement.value = 'Cambio desde el ts';
+  }
 }
