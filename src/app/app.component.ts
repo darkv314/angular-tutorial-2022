de30b7c614332230ac216b2579e3cea13acedbfd
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-22 03:02:49 -0400

    Task 3 Outputs, hostListener in attributes directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'test';
   color: string = 'red';
+
+  getMessage(event: any) {
+    console.log('Evento al padre: ', event);
+  }
 }
--- src/app/over-upb.directive.ts
+++ src/app/over-upb.directive.ts
@@ -1,14 +1,42 @@
-import { Directive, ElementRef, Input, OnChanges, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Directive({
   selector: '[overUpb]',
 })
 export class OverUpbDirective implements OnChanges {
   @Input() overUpb!: string;
+  @Output() sendMessage = new EventEmitter<any>();
+
+  // @Output() overUpb = new EventEmitter<any>()
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.element.nativeElement.style.backgroundColor = 'red';
+  }
+
+  @HostListener('mouseleave', ['$event']) onMouseLeave(event: any) {
+    this.element.nativeElement.style.backgroundColor = 'green';
+    console.log('event al salir: ', event);
+    this.sendMessage.emit({
+      id: 0,
+      text: 'saliste del elemento',
+      x: event.x,
+      y: event.y,
+    });
+  }
+
   constructor(private element: ElementRef) {}
 
   ngOnChanges() {
-    console.log(this.element); 
+    console.log(this.element);
     this.element.nativeElement.style.backgroundColor = this.overUpb;
   }
 }
