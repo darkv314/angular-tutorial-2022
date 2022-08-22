1050768c9d1d74abcaa0c6408b24744be50fd32d
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-21 20:44:01 -0400

    Task1 ngAfter, content init, content checked, view init, view checked

---
+++ src/app/person/person.component.ts
@@ -0,0 +1,39 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css'],
+})
+export class PersonComponent implements OnInit {
+  @Input() name: string = '';
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+}
