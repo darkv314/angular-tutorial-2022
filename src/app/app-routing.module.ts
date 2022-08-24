4e6504b44edc642faec7360a8c4423b149c72d95
darkv314 <ricardo.tejerina314@gmail.com>
2022-08-24 17:15:09 -0400

    Task 1 Basic navigaton with components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { View1Component } from './dashboard/view1/view1.component';
+import { View2Component } from './dashboard/view2/view2.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    title: 'Login',
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    title: 'Dashboard',
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'view1',
+        component: View1Component,
+      },
+      {
+        path: 'view2',
+        component: View2Component,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { View1Component } from './dashboard/view1/view1.component';
+import { View2Component } from './dashboard/view2/view2.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    View1Component,
+    View2Component,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
