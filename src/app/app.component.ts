6715eaeae5bcb12d8498b3af1d4f7a199bf8db23
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2022-09-04 19:51:10 -0400

    Task 4 sending query params from html and ts, nulish coallesing, or, ternario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,19 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'test';
   id: number =1;
-  constructor(private router: Router) {}
+  var1: any = 0;
+  var2: any = 2;
+  constructor(private router: Router) {
+    console.log('nulish coalesing:',     this.var1 ?? this.var2    )
+    console.log('Or', this.var1 || this.var2)
+
+    //if(1 === 1) {
+    //  return 1 +1 
+    //} else {
+    //  return 3+5
+    //}
+    console.log('ternario:', 1 === 1 ? 1 : null )
+  }
 
   goView1() {
     this.router.navigate(['view1', this.id])
@@ -17,5 +29,17 @@ export class AppComponent {
 
   goView2() {
     this.router.navigate(['view2', this.id, 'listNames', this.id])
+  }
+
+  goView3() {
+    this.router.navigate(
+      ['view3'],
+      {
+        queryParams: {
+          name: 'maria',
+          lastName: this.id
+        }
+      }
+    )
   }
 }
