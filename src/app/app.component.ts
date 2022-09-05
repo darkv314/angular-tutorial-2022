09e46b927bc5feba3f311f2e6729fcafc4f15d08
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2022-09-05 03:43:19 -0400

    Task 2 singleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  constructor(private userService:UserService) {
+
+  }
+
+  getToken() {
+    console.log(this.userService.getToken());
+  }
 }
