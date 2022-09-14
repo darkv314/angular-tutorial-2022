3a3c3b9b9fc0f4c718e48bac3f69a75f4a97358f
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2022-09-14 08:58:47 -0400

    Task 3 example form group in reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms'
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -14,14 +14,38 @@ export class AppComponent implements OnInit{
   }
 
   public colorControl = new FormControl('');
+  public personForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.personForm = this.formBuilder.group({
+      name: '',
+      lastName: '',
+      age: 0,
+      country: ''
+    })
+  }
 
   ngOnInit() {
       this.colorControl.valueChanges.subscribe(value =>{
         console.log("Value changes: ", value)
       })
+
+      this.personForm.valueChanges.subscribe(values => {
+        console.log("Values: ", values)
+      })
   }
 
   printColorControl(){
     console.log(this.colorControl)
+  }
+
+  sendPerson() {
+    console.log('Reactive form: ', this.personForm)
+  }
+  disableName() {
+    this.personForm.get('name')?.disable();
+  }
+  enableName() {
+    this.personForm.get('name')?.enable();
   }
 }
