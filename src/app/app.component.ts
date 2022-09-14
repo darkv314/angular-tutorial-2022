7f8fd7ee2aaac6d529a874c256b1e4c5ae8b564c
Ricardo Tejerina <ricardo.tejerina314@gmail.com>
2022-09-14 11:13:02 -0400

    08-09-2022 Task 1 basic config of project course fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'test';
 
-  constructor(private testService: TestService){
-    console.log(this.testService.getName())
+  constructor(){
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from "@angular/material/button"
-import { TestService } from './test.service';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  }
+];
 
 
 @NgModule({
@@ -14,12 +24,8 @@ import { TestService } from './test.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule 
-  ],
-  providers: [
-    TestService
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent]
 })
